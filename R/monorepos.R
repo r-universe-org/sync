f898bfab917c2d8c71d0620ad615a4b1d4cb8752
Jeroen Ooms <jeroenooms@gmail.com>
2020-10-30 00:33:47 +0100

    add support for remotes

--- R/monorepos.R
+++ R/monorepos.R
@@ -32,15 +32,8 @@ sync_from_registry <- function(monorepo_url = Sys.getenv('MONOREPO_URL')){
   # Sync with the user registry file
   sys::exec_wait("git", c("submodule", "update", "--init", "--remote", '.registry'))
   gert::git_reset_hard('origin/HEAD', repo = I('.registry'))
-  jsonfile <- sprintf('.registry/%s.json', sub("_", "@", basename(monorepo_url), fixed = TRUE))
-  registry <- if(file.exists(jsonfile)){
-    jsonlite::read_json(jsonfile)
-  } else {
-    jsonlite::read_json('.registry/packages.json')
-  }
-  registry_url <- gert::git_remote_list(repo = I('.registry'))$url
-  generate_gitmodules(pkgs = registry, registry_url = registry_url)
   registry_commit <- gert::git_log(repo = I('.registry'), max = 1)
+  update_gitmodules()
   gert::git_add('.gitmodules')
   if(any(gert::git_status()$staged)){
     print_message("Sync registry with upstream")
@@ -52,8 +45,16 @@ sync_from_registry <- function(monorepo_url = Sys.getenv('MONOREPO_URL')){
     print_message("Registry is up-to-date")
   }
 
-  # First get rid of deleted packages
-  packages <- vapply(registry, function(x){x$package}, character(1))
+  # First update all packages from the registry
+  registry <- read_registry_list()
+  lapply(registry, update_one_package)
+
+  # Now update all remotes (possibly new ones from package updates)
+  remotes <- read_remotes_list()
+  lapply(remotes, update_one_package)
+
+  # Finally get rid of deleted packages
+  packages <- vapply(c(registry, remotes), function(x){x$package}, character(1))
   remove_packages <- setdiff(list.files(repo), packages)
   if(length(remove_packages)){
     lapply(remove_packages, function(pkg_dir){
@@ -64,61 +65,113 @@ sync_from_registry <- function(monorepo_url = Sys.getenv('MONOREPO_URL')){
     msg <- paste("Deleting packages:", paste0(remove_packages, collapse = ', '))
     gert::git_commit(msg, registry_commit$author)
     gert::git_push()
+  } else {
+    print_message("No packages to delete. Everything is up-to-date")
   }
+}
 
-  # Update all the submodules!
-  # sys::exec_wait("git", c("submodule", "update", "--init", "--recursive", "--remote", "--jobs", "8"))
-  # Todo: can we update the module without cloning?
-
-  # Sync the registry packages with the monorepo
-  lapply(registry, function(x){
-    pkg_dir <- x$package
-    pkg_url <- x$url
-    if(isFALSE(x$available)){
-      print_message("Skipping unavailable package %s", pkg_dir)
+# Sync the registry packages with the monorepo
+update_one_package <- function(x){
+  pkg_dir <- x$package
+  pkg_url <- x$url
+  if(isFALSE(x$available)){
+    print_message("Skipping unavailable package %s", pkg_dir)
+    return()
+  }
+  submodule <- sys::exec_internal("git", c("submodule", "status", pkg_dir), error = FALSE)
+  if(submodule$status == 0){
+    branch <- ifelse(length(x$branch) > 0, x$branch, 'HEAD')
+    out <- sys::exec_internal('git', c("ls-remote", pkg_url, branch))
+    remote_head <- strsplit(sys::as_text(out$stdout), '\\W')[[1]][1]
+    if(grepl(remote_head, sys::as_text(submodule$stdout), fixed = TRUE)){
+      print_message("Submodule %s unchanged (%s)", pkg_dir, remote_head)
       return()
     }
-    submodule <- sys::exec_internal("git", c("submodule", "status", pkg_dir), error = FALSE)
-    if(submodule$status == 0){
-      branch <- ifelse(length(x$branch) > 0, x$branch, 'HEAD')
-      out <- sys::exec_internal('git', c("ls-remote", pkg_url, branch))
-      remote_head <- strsplit(sys::as_text(out$stdout), '\\W')[[1]][1]
-      if(grepl(remote_head, sys::as_text(submodule$stdout), fixed = TRUE)){
-        print_message("Submodule %s unchanged (%s)", pkg_dir, remote_head)
-        return()
-      }
-      print_message("Updating package '%s' from: %s", pkg_dir, pkg_url)
-      sys::exec_wait("git", c("submodule", "update", "--init", "--remote", pkg_dir))
-    } else {
-      print_message("Adding new package '%s' from: %s", pkg_dir, pkg_url)
-      sys::exec_wait("git", c("submodule", "add", "--force", pkg_url, pkg_dir))
+    print_message("Updating package '%s' from: %s", pkg_dir, pkg_url)
+    sys::exec_wait("git", c("submodule", "update", "--init", "--remote", pkg_dir))
+  } else {
+    print_message("Adding new package '%s' from: %s", pkg_dir, pkg_url)
+    sys::exec_wait("git", c("submodule", "add", "--force", pkg_url, pkg_dir))
+  }
+  gert::git_reset_hard('origin/HEAD', repo = I(pkg_dir))
+  gert::git_add(pkg_dir)
+  if(!any(gert::git_status()$staged)){
+    print_message("Submodule '%s' already up-to-date", pkg_dir)
+  } else {
+    desc <- get_description_data(pkg_dir)
+    update_remotes_json(desc)
+    update_gitmodules()
+    gert::git_add(c('.remotes.json', '.gitmodules'))
+    subrepo <- gert::git_open(pkg_dir)
+    stopifnot(basename(gert::git_info(repo = subrepo)$path) == pkg_dir)
+    pkg_commit <- gert::git_log(repo = subrepo, max = 1)
+    sig <- paste(trimws(desc$maintainer), unclass(pkg_commit$time))
+    gert::git_commit(message = paste(desc$package, desc$version), author = sig)
+    gert::git_push()
+  }
+}
+
+update_remotes_json <- function(desc){
+  new_remotes <- get_package_remotes(desc)
+  other_remotes <- Filter(function(x){
+    (x$from != desc$package)
+  }, read_remotes_list())
+  all_remotes <- c(other_remotes, new_remotes)
+  if(length(all_remotes)){
+    pkg_names <- vapply(all_remotes, function(x){x$package}, character(1))
+    jsonlite::write_json(all_remotes[order(pkg_names)], '.remotes.json',
+                         auto_unbox = TRUE, pretty = TRUE)
+  } else {
+    unlink('.remotes.json')
+  }
+}
+
+# TODO: download description to check availability and recursive remotes
+get_package_remotes <- function(desc){
+  if(!length(desc$remotes) || !nchar(desc$remotes))
+    return(NULL)
+  remotes_repos <- trimws(strsplit(desc$remotes, ',')[[1]])
+  lapply(remotes_repos, function(x){
+    info <- remotes::parse_repo_spec(x)
+    out <- list(
+      package = info$repo,
+      url = sprintf("https://github.com/%s/%s", info$username, info$repo),
+      from = desc$package
+    )
+    if(length(info$ref) && nchar(info$ref)){
+      out$branch = info$ref
     }
-    gert::git_reset_hard('origin/HEAD', repo = I(pkg_dir))
-    gert::git_add(pkg_dir)
-    if(!any(gert::git_status()$staged)){
-      print_message("Submodule '%s' already up-to-date", pkg_dir)
-    } else {
-      desc <- get_description_data(pkg_dir)
-      subrepo <- gert::git_open(pkg_dir)
-      stopifnot(basename(gert::git_info(repo = subrepo)$path) == pkg_dir)
-      pkg_commit <- gert::git_log(repo = subrepo, max = 1)
-      sig <- paste(trimws(desc$maintainer), unclass(pkg_commit$time))
-      gert::git_commit(message = paste(desc$package, desc$version), author = sig)
-      gert::git_push()
-    }
+    return(out)
   })
-  invisible(packages)
+}
+
+read_remotes_list <- function(){
+  if(file.exists('.remotes.json'))
+    jsonlite::read_json('.remotes.json')
 }
 
 print_message <- function(...){
   message(sprintf(...))
 }
 
-generate_gitmodules <- function(pkgs, registry_url){
+read_registry_list <- function(){
+  monorepo_url <- gert::git_remote_info()$url
+  jsonfile <- sprintf('.registry/%s.json', sub("_", "@", basename(monorepo_url), fixed = TRUE))
+  if(file.exists(jsonfile)){
+    jsonlite::read_json(jsonfile)
+  } else {
+    jsonlite::read_json('.registry/packages.json')
+  }
+}
+
+update_gitmodules <- function(){
+  registry <- read_registry_list()
+  remotes <- read_remotes_list()
+  registry_url <- gert::git_remote_list(repo = I('.registry'))$url
   pkgs <- c(list(list(
     package = '.registry',
     url = registry_url
-  )), pkgs)
+  )), registry, remotes)
   lines <- vapply(pkgs, function(x){
     if(!length(x$package))
       stop("Field 'package' missing from registry entry")
