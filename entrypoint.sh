3fe0a27a2598274ccde7969a3bb3f0f09132c71a
Jeroen Ooms <jeroenooms@gmail.com>
2022-06-01 16:51:54 +0200

    Test new script as action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -l
 set -e
 echo "Synchronizing ${1} in ${PWD}"
+if [ "${1}" == "" ]; then
+Rscript -e "sync::update_local()"
+else
 Rscript -e "sync::sync_from_registry('${1}')"
+fi
 echo "Action complete!"
