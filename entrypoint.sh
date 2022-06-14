20e54f0a43813fdce2a95be4d7c1a138b41634da
Jeroen Ooms <jeroenooms@gmail.com>
2022-06-14 14:49:09 +0200

    Add an action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -l
 set -e
 echo "Synchronizing ${1} in ${PWD}"
-if [ "$1" == "https://github.com/r-universe/cran" ] || [ "$2" == "true" ]; then
+if [ "$1" == "all" ]; then
+Rscript -e "sync::trigger_syncs()"
+elif [ "$1" == "https://github.com/r-universe/cran" ] || [ "$2" == "true" ]; then
 Rscript -e "sync::update_remote('${1}')"
 else
 Rscript -e "sync::sync_from_registry('${1}')"
