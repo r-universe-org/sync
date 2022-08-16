249526266c055f3dc5c980de8edbf9f336df1ea3
Jeroen Ooms <jeroenooms@gmail.com>
2022-08-16 20:31:15 +0200

    Force re-check for "*release" entries

--- R/monorepos.R
+++ R/monorepos.R
@@ -90,8 +90,11 @@ sync_from_registry <- function(monorepo_url = Sys.getenv('MONOREPO_URL')){
 
   # First update all packages from the registry
   skiplist <- submodules_up_to_date()
-  print_message("Submodules up-to-date:\n %s", paste(skiplist, collapse = '\n '))
   registry <- read_registry_list()
+  forcecheck <- sapply(Filter(function(x){identical(x$branch, '*release')}, registry), function(x){x$package})
+  skiplist <- setdiff(skiplist, forcecheck)
+
+  print_message("Submodules up-to-date:\n %s", paste(skiplist, collapse = '\n '))
   dirty <- Filter(function(x){is.na(match(x$package, skiplist))}, registry)
   results1 <- lapply(dirty, try_update_package, update_pkg_remotes = TRUE)
 
