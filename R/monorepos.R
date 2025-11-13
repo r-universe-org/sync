555bf60adfc536d63e89a94d0222122502a4b890
Jeroen Ooms <jeroenooms@gmail.com>
2025-11-13 13:32:47 +0100

    Bioc: allow deprecated packges in 'release'

--- R/monorepos.R
+++ R/monorepos.R
@@ -870,7 +870,7 @@ metabioc_devel_registry <- function(){
   bioc_version <- yml$devel_version
   bioc <- jsonlite::read_json(sprintf('https://bioconductor.org/packages/json/%s/bioc/packages.json', bioc_version))
   stopifnot(length(bioc) > 2100)
-  bioc <- Filter(function(x) !identical(x$PackageStatus, 'Deprecated') || identical(x$Package, 'zlibbioc'), bioc)
+  bioc <- Filter(function(x) !identical(x$PackageStatus, 'Deprecated'), bioc)
   lapply(setdiff(names(bioc), skiplist), function(x){
     baseurl <- ifelse(x %in% nomirror, "https://git.bioconductor.org/packages/", "https://github.com/bioc/")
     list(package = x, url = paste0(baseurl, x ))
@@ -883,7 +883,7 @@ metabioc_release_registry <- function(){
   bioc_version <- yml$release_version
   bioc <- jsonlite::read_json(sprintf('https://bioconductor.org/packages/json/%s/bioc/packages.json', bioc_version))
   stopifnot(length(bioc) > 2100)
-  bioc <- Filter(function(x) !identical(x$PackageStatus, 'Deprecated') || identical(x$Package, 'zlibbioc'), bioc)
+  #bioc <- Filter(function(x) !identical(x$PackageStatus, 'Deprecated'), bioc)
   lapply(bioc, function(pkginfo){
     x <- pkginfo$Package
     baseurl <- ifelse(x %in% nomirror, "https://git.bioconductor.org/packages/", "https://github.com/bioc/")
