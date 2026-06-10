278caf807d198fa8427f8b440b6fe3de70f2d5a1
Jeroen Ooms <jeroenooms@gmail.com>
2026-06-10 23:42:43 +0200

    Improve errors for repo auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -l
 set -e
+
+# Authenticating gives better error messages for git
+export GIT_TERMINAL_PROMPT=0
+if [ "$GITHUB_PAT" ]; then
+AUTH=$(printf "x-access-token:%s" "$GITHUB_PAT" | base64 -w0)
+git config --global http.https://github.com/.extraheader "AUTHORIZATION: basic $AUTH"
+fi
+
 echo "Synchronizing ${1} in ${PWD}"
 if [ "$1" == "all" ]; then
 Rscript -e "sync::trigger_syncs()"
